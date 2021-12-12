9869110b13ec86a76769012a0d7a2a316d56800d
A-ASolutions <69863696+A-ASolutions@users.noreply.github.com>
2021-12-12 14:58:33 +0000

    Project ready

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+
+// Prompt to select media stream,pass to video element
+async function selectMediaStream() {
+    try {
+        const mediaStreatm = await navigator.mediaDevices.getDisplayMedia();
+
+        videoElement.srcObject = mediaStreatm;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        };
+    } catch (error) {
+        // catch error here
+    }
+}
+button.addEventListener('click', async() => {
+    //disable the button
+
+    button.disabled = true;
+
+    // Start picture in picture
+
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+})
+
+//
+selectMediaStream();
